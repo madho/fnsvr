9296eb26038393798ea18069447f18b2597a0a69
madho <jasonmadhosingh@gmail.com>
2026-03-28 16:31:32 -0400

    fix(04-02): add resource stanzas to Homebrew formula for Python dependencies
    SHA256 hashes are placeholders until PyPI publish. Regenerate with:
      pip install homebrew-pypi-poet && poet fnsvr

--- homebrew/fnsvr.rb
+++ homebrew/fnsvr.rb
@@ -13,6 +13,34 @@ class Fnsvr < Formula
 
   depends_on "python@3.12"
 
+  # Resource stanzas generated via: pip install homebrew-pypi-poet && poet fnsvr
+  # SHA256 hashes are placeholders until PyPI publish. Regenerate with:
+  #   poet fnsvr > resources.rb
+  resource "click" do
+    url "https://pypi.io/packages/source/c/click/click-8.1.7.tar.gz"
+    sha256 "PLACEHOLDER_CLICK_SHA256"
+  end
+
+  resource "google-api-python-client" do
+    url "https://pypi.io/packages/source/g/google-api-python-client/google-api-python-client-2.131.0.tar.gz"
+    sha256 "PLACEHOLDER_GOOGLE_API_SHA256"
+  end
+
+  resource "google-auth-httplib2" do
+    url "https://pypi.io/packages/source/g/google-auth-httplib2/google-auth-httplib2-0.2.0.tar.gz"
+    sha256 "PLACEHOLDER_GOOGLE_AUTH_HTTPLIB2_SHA256"
+  end
+
+  resource "google-auth-oauthlib" do
+    url "https://pypi.io/packages/source/g/google-auth-oauthlib/google-auth-oauthlib-1.2.0.tar.gz"
+    sha256 "PLACEHOLDER_GOOGLE_AUTH_OAUTHLIB_SHA256"
+  end
+
+  resource "PyYAML" do
+    url "https://pypi.io/packages/source/p/PyYAML/PyYAML-6.0.1.tar.gz"
+    sha256 "PLACEHOLDER_PYYAML_SHA256"
+  end
+
   def install
     virtualenv_install_with_resources
   end
